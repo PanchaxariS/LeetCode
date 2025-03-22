7b9dda6c25f18d60bb00c447ff61a5ad3f6094b2
Panchaxari Shanavadmeti <121090701+PanchaxariS@users.noreply.github.com>
2025-03-22 12:01:32 +0530

    Time: 1 ms (83.87%), Space: 40.8 MB (71.35%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,13 +3,10 @@ class Solution {
         int result = 0;
 
         while (x != 0) {
-            int remainder = x % 10;
+            if (result > 214748364 || result < -214748364) return 0;
 
-            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && remainder > 7)) return 0;
-            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && remainder < -8)) return 0;
-
-            result = result * 10 + remainder;
-            x = x / 10;
+            result = result * 10 + x % 10;
+            x /= 10;
         }
 
         return result;
